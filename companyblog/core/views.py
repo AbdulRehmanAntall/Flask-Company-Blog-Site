97c2dd29051c7f666f638b10389643c87e5d578e
AbdulRehmanAntall <abdulrehmanantall420@gmail.com>
2025-07-12 21:49:11 +0500

    Remove venv folder from repo

---
+++ companyblog/core/views.py
@@ -0,0 +1,11 @@
+from flask import render_template, redirect, Blueprint
+
+core = Blueprint('core', __name__)  # no leading slash in blueprint name!
+
+@core.route('/')
+def index():
+    return render_template('index.html')
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
