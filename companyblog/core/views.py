4109819349d32150d7542dfbb1777660d5af8325
AbdulRehmanAntall <abdulrehmanantall420@gmail.com>
2025-07-14 22:28:28 +0500

    Version1: Final Commit

--- companyblog/core/views.py
+++ companyblog/core/views.py
@@ -1,13 +1,40 @@
-from flask import render_template,request, redirect, Blueprint
-from companyblog.models import BlogPost
-core = Blueprint('core', __name__)  # no leading slash in blueprint name!
+from flask import render_template, request, redirect, Blueprint
+from companyblog.models import BlogPost,User
+from sqlalchemy import or_
+
+
+core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    page=request.args.get('page',1,type=int)
-    blog_posts=BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page,per_page=5)
-    return render_template('index.html',blog_posts=blog_posts)
+    page = request.args.get('page', 1, type=int)
+    search_query = request.args.get('search', '')
+
+    if search_query:
+        blog_posts = BlogPost.query.filter(
+            or_(
+                BlogPost.title.ilike(f'%{search_query}%'),
+                BlogPost.text.ilike(f'%{search_query}%')
+            )
+        ).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    else:
+        blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+
+    return render_template('index.html', blog_posts=blog_posts)
 
 @core.route('/info')
 def info():
     return render_template('info.html')
+
+@core.route('/find-people', methods=['GET', 'POST'])
+def find_people():
+    search_query = request.args.get('search', '')
+
+    # For demo: query users by username containing search_query (case-insensitive)
+    if search_query:
+        users = User.query.filter(User.username.ilike(f'%{search_query}%')).all()
+    else:
+        users = []
+
+    return render_template('findpeople.html', users=users, search_query=search_query)
+
